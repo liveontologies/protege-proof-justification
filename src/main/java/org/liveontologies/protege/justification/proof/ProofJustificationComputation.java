1609468ae677ee7a666fcafcb1d0709c354958f2
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2017-06-23 16:33:34 +0200

    Use the preferred justification priority

--- src/main/java/org/liveontologies/protege/justification/proof/ProofJustificationComputation.java
+++ src/main/java/org/liveontologies/protege/justification/proof/ProofJustificationComputation.java
@@ -4,10 +4,12 @@ import java.util.Set;
 
 import org.liveontologies.protege.explanation.justification.service.JustificationComputation;
 import org.liveontologies.protege.explanation.justification.service.JustificationListener;
+import org.liveontologies.protege.explanation.justification.service.JustificationPriorityComparator;
 import org.liveontologies.protege.justification.proof.service.JustificationCompleteProof;
 import org.liveontologies.puli.justifications.InterruptMonitor;
 import org.liveontologies.puli.justifications.MinimalSubsetEnumerator;
 import org.liveontologies.puli.justifications.MinimalSubsetEnumerators;
+import org.liveontologies.puli.justifications.PriorityComparator;
 import org.semanticweb.owlapi.model.OWLAxiom;
 
 public class ProofJustificationComputation extends JustificationComputation
@@ -15,6 +17,8 @@ public class ProofJustificationComputation extends JustificationComputation
 		MinimalSubsetEnumerator.Listener<OWLAxiom> {
 
 	private final JustificationProofManager manager_;
+
+	private PriorityComparator<Set<OWLAxiom>, ?> priorityCompatator_;
 
 	public ProofJustificationComputation(JustificationProofManager manager,
 			JustificationListener listener,
@@ -24,14 +28,37 @@ public class ProofJustificationComputation extends JustificationComputation
 	}
 
 	@Override
+	public <P> boolean setPrefferredPriority(
+			JustificationPriorityComparator<P> comparator) {
+		this.priorityCompatator_ = new PriorityComparator<Set<OWLAxiom>, P>() {
+
+			@Override
+			public int compare(P p1, P p2) {
+				return comparator.compare(p1, p2);
+			}
+
+			@Override
+			public P getPriority(Set<OWLAxiom> justification) {
+				return comparator.getPriority(justification);
+			}
+		};
+		return true;
+	}
+
+	@Override
 	public void startComputation() {
 		enumerateJustifications(manager_.getProof());
 	}
 
 	public <C> void enumerateJustifications(
 			JustificationCompleteProof<C> proof) {
-		MinimalSubsetEnumerators.enumerateJustifications(proof.getGoal(), proof,
-				proof, this, this);
+		if (priorityCompatator_ == null) {
+			MinimalSubsetEnumerators.enumerateJustifications(proof.getGoal(),
+					proof, proof, this, this);
+		} else {
+			MinimalSubsetEnumerators.enumerateJustifications(proof.getGoal(),
+					proof, proof, priorityCompatator_, this, this);
+		}
 	}
 
 	@Override
