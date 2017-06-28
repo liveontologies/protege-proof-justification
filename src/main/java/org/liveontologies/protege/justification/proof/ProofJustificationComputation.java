26ac31cadc27645c93736ec338583e2f2cfd3497
Peter Skocovsky <peter.skocovsky@uni-ulm.de>
2017-06-28 18:34:20 +0200

    Rename the justification package

--- src/main/java/org/liveontologies/protege/justification/proof/ProofJustificationComputation.java
+++ src/main/java/org/liveontologies/protege/justification/proof/ProofJustificationComputation.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * Protege Proof Justification
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2016 - 2017 Live Ontologies Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package org.liveontologies.protege.justification.proof;
 
 import java.util.Set;
@@ -6,10 +27,10 @@ import org.liveontologies.protege.explanation.justification.service.Justificatio
 import org.liveontologies.protege.explanation.justification.service.JustificationListener;
 import org.liveontologies.protege.explanation.justification.service.JustificationPriorityComparator;
 import org.liveontologies.protege.justification.proof.service.JustificationCompleteProof;
-import org.liveontologies.puli.justifications.InterruptMonitor;
-import org.liveontologies.puli.justifications.MinimalSubsetEnumerator;
-import org.liveontologies.puli.justifications.MinimalSubsetEnumerators;
-import org.liveontologies.puli.justifications.PriorityComparator;
+import org.liveontologies.puli.pinpointing.InterruptMonitor;
+import org.liveontologies.puli.pinpointing.MinimalSubsetEnumerator;
+import org.liveontologies.puli.pinpointing.MinimalSubsetEnumerators;
+import org.liveontologies.puli.pinpointing.PriorityComparator;
 import org.semanticweb.owlapi.model.OWLAxiom;
 
 public class ProofJustificationComputation extends JustificationComputation
